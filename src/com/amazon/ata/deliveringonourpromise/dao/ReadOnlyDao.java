9be94e845580f89ef0840e4031d6c5523c192f49
Dicerson <isaacgoodloe@gmail.com>
2024-07-23 03:57:46 -0700

    Checkstyle fix, bad starting files tricked web machine into thinking a variable was an HTML tag

--- src/com/amazon/ata/deliveringonourpromise/dao/ReadOnlyDao.java
+++ src/com/amazon/ata/deliveringonourpromise/dao/ReadOnlyDao.java
@@ -2,11 +2,11 @@ package com.amazon.ata.deliveringonourpromise.dao;
 /**
  * DAO interface to abstract calls.
  */
-public interface ReadOnlyDao<I, O> {
+public interface ReadOnlyDao<X, Y> {
     /**
      * Get object method to be implemented.
      * @param orderId Order Id
      * @return Object abstracted object
      */
-    O get(I orderId);
+    Y get(X orderId);
 }
